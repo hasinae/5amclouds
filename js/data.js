99e45f7dab0ebba3199285a553ba2dcce341be99
hasina <113416278+hasinae@users.noreply.github.com>
2026-08-31 19:33:15 -0700

    first commit

---
+++ js/data.js
@@ -0,0 +1,578 @@
+const GALLERY_IMAGES = [
+  {
+    "id": 1,
+    "src": "images/IMG_1366.jpg",
+    "thumb": "images/thumbs/IMG_1366.jpg",
+    "title": "No. 01",
+    "category": "5AMCLOUDS \u2014 FRAME",
+    "description": "",
+    "file": "IMG_1366.jpg"
+  },
+  {
+    "id": 2,
+    "src": "images/IMG_1524.jpg",
+    "thumb": "images/thumbs/IMG_1524.jpg",
+    "title": "No. 02",
+    "category": "5AMCLOUDS \u2014 FRAME",
+    "description": "",
+    "file": "IMG_1524.jpg"
+  },
+  {
+    "id": 3,
+    "src": "images/IMG_1834.jpg",
+    "thumb": "images/thumbs/IMG_1834.jpg",
+    "title": "No. 03",
+    "category": "5AMCLOUDS \u2014 FRAME",
+    "description": "",
+    "file": "IMG_1834.jpg"
+  },
+  {
+    "id": 4,
+    "src": "images/IMG_1959.jpg",
+    "thumb": "images/thumbs/IMG_1959.jpg",
+    "title": "No. 04",
+    "category": "5AMCLOUDS \u2014 FRAME",
+    "description": "",
+    "file": "IMG_1959.jpg"
+  },
+  {
+    "id": 5,
+    "src": "images/IMG_1977.jpg",
+    "thumb": "images/thumbs/IMG_1977.jpg",
+    "title": "No. 05",
+    "category": "5AMCLOUDS \u2014 FRAME",
+    "description": "",
+    "file": "IMG_1977.jpg"
+  },
+  {
+    "id": 6,
+    "src": "images/IMG_2009.jpg",
+    "thumb": "images/thumbs/IMG_2009.jpg",
+    "title": "No. 06",
+    "category": "5AMCLOUDS \u2014 FRAME",
+    "description": "",
+    "file": "IMG_2009.jpg"
+  },
+  {
+    "id": 7,
+    "src": "images/IMG_2261.jpg",
+    "thumb": "images/thumbs/IMG_2261.jpg",
+    "title": "No. 07",
+    "category": "5AMCLOUDS \u2014 FRAME",
+    "description": "",
+    "file": "IMG_2261.jpg"
+  },
+  {
+    "id": 8,
+    "src": "images/IMG_2298.jpg",
+    "thumb": "images/thumbs/IMG_2298.jpg",
+    "title": "No. 08",
+    "category": "5AMCLOUDS \u2014 FRAME",
+    "description": "",
+    "file": "IMG_2298.jpg"
+  },
+  {
+    "id": 9,
+    "src": "images/IMG_2308.jpg",
+    "thumb": "images/thumbs/IMG_2308.jpg",
+    "title": "No. 09",
+    "category": "5AMCLOUDS \u2014 FRAME",
+    "description": "",
+    "file": "IMG_2308.jpg"
+  },
+  {
+    "id": 10,
+    "src": "images/IMG_2446.jpg",
+    "thumb": "images/thumbs/IMG_2446.jpg",
+    "title": "No. 10",
+    "category": "5AMCLOUDS \u2014 FRAME",
+    "description": "",
+    "file": "IMG_2446.jpg"
+  },
+  {
+    "id": 11,
+    "src": "images/IMG_2481.jpg",
+    "thumb": "images/thumbs/IMG_2481.jpg",
+    "title": "No. 11",
+    "category": "5AMCLOUDS \u2014 FRAME",
+    "description": "",
+    "file": "IMG_2481.jpg"
+  },
+  {
+    "id": 12,
+    "src": "images/IMG_2513.jpg",
+    "thumb": "images/thumbs/IMG_2513.jpg",
+    "title": "No. 12",
+    "category": "5AMCLOUDS \u2014 FRAME",
+    "description": "",
+    "file": "IMG_2513.jpg"
+  },
+  {
+    "id": 13,
+    "src": "images/IMG_2754.jpg",
+    "thumb": "images/thumbs/IMG_2754.jpg",
+    "title": "No. 13",
+    "category": "5AMCLOUDS \u2014 FRAME",
+    "description": "",
+    "file": "IMG_2754.jpg"
+  },
+  {
+    "id": 14,
+    "src": "images/IMG_3160.jpg",
+    "thumb": "images/thumbs/IMG_3160.jpg",
+    "title": "No. 14",
+    "category": "5AMCLOUDS \u2014 FRAME",
+    "description": "",
+    "file": "IMG_3160.jpg"
+  },
+  {
+    "id": 15,
+    "src": "images/IMG_3303.jpg",
+    "thumb": "images/thumbs/IMG_3303.jpg",
+    "title": "No. 15",
+    "category": "5AMCLOUDS \u2014 FRAME",
+    "description": "",
+    "file": "IMG_3303.jpg"
+  },
+  {
+    "id": 16,
+    "src": "images/IMG_3940.jpg",
+    "thumb": "images/thumbs/IMG_3940.jpg",
+    "title": "No. 16",
+    "category": "5AMCLOUDS \u2014 FRAME",
+    "description": "",
+    "file": "IMG_3940.jpg"
+  },
+  {
+    "id": 17,
+    "src": "images/IMG_3954.jpg",
+    "thumb": "images/thumbs/IMG_3954.jpg",
+    "title": "No. 17",
+    "category": "5AMCLOUDS \u2014 FRAME",
+    "description": "",
+    "file": "IMG_3954.jpg"
+  },
+  {
+    "id": 18,
+    "src": "images/IMG_3959.jpg",
+    "thumb": "images/thumbs/IMG_3959.jpg",
+    "title": "No. 18",
+    "category": "5AMCLOUDS \u2014 FRAME",
+    "description": "",
+    "file": "IMG_3959.jpg"
+  },
+  {
+    "id": 19,
+    "src": "images/IMG_3960.jpg",
+    "thumb": "images/thumbs/IMG_3960.jpg",
+    "title": "No. 19",
+    "category": "5AMCLOUDS \u2014 FRAME",
+    "description": "",
+    "file": "IMG_3960.jpg"
+  },
+  {
+    "id": 20,
+    "src": "images/IMG_3965.jpg",
+    "thumb": "images/thumbs/IMG_3965.jpg",
+    "title": "No. 20",
+    "category": "5AMCLOUDS \u2014 FRAME",
+    "description": "",
+    "file": "IMG_3965.jpg"
+  },
+  {
+    "id": 21,
+    "src": "images/IMG_4028.jpg",
+    "thumb": "images/thumbs/IMG_4028.jpg",
+    "title": "No. 21",
+    "category": "5AMCLOUDS \u2014 FRAME",
+    "description": "",
+    "file": "IMG_4028.jpg"
+  },
+  {
+    "id": 22,
+    "src": "images/IMG_4113.jpg",
+    "thumb": "images/thumbs/IMG_4113.jpg",
+    "title": "No. 22",
+    "category": "5AMCLOUDS \u2014 FRAME",
+    "description": "",
+    "file": "IMG_4113.jpg"
+  },
+  {
+    "id": 23,
+    "src": "images/IMG_extra1.jpg",
+    "thumb": "images/thumbs/IMG_extra1.jpg",
+    "title": "No. 23",
+    "category": "5AMCLOUDS \u2014 FRAME",
+    "description": "",
+    "file": "IMG_extra1.jpg"
+  },
+  {
+    "id": 24,
+    "src": "images/IMG_4604.jpg",
+    "thumb": "images/thumbs/IMG_4604.jpg",
+    "title": "No. 24",
+    "category": "5AMCLOUDS \u2014 FRAME",
+    "description": "",
+    "file": "IMG_4604.jpg"
+  },
+  {
+    "id": 25,
+    "src": "images/IMG_4620.jpg",
+    "thumb": "images/thumbs/IMG_4620.jpg",
+    "title": "No. 25",
+    "category": "5AMCLOUDS \u2014 FRAME",
+    "description": "",
+    "file": "IMG_4620.jpg"
+  },
+  {
+    "id": 26,
+    "src": "images/IMG_4624.jpg",
+    "thumb": "images/thumbs/IMG_4624.jpg",
+    "title": "No. 26",
+    "category": "5AMCLOUDS \u2014 FRAME",
+    "description": "",
+    "file": "IMG_4624.jpg"
+  },
+  {
+    "id": 27,
+    "src": "images/IMG_4626.jpg",
+    "thumb": "images/thumbs/IMG_4626.jpg",
+    "title": "No. 27",
+    "category": "5AMCLOUDS \u2014 FRAME",
+    "description": "",
+    "file": "IMG_4626.jpg"
+  },
+  {
+    "id": 28,
+    "src": "images/IMG_4627.jpg",
+    "thumb": "images/thumbs/IMG_4627.jpg",
+    "title": "No. 28",
+    "category": "5AMCLOUDS \u2014 FRAME",
+    "description": "",
+    "file": "IMG_4627.jpg"
+  },
+  {
+    "id": 29,
+    "src": "images/IMG_extra2.jpg",
+    "thumb": "images/thumbs/IMG_extra2.jpg",
+    "title": "No. 29",
+    "category": "5AMCLOUDS \u2014 FRAME",
+    "description": "",
+    "file": "IMG_extra2.jpg"
+  },
+  {
+    "id": 30,
+    "src": "images/6146A82A-D477-43AF-8ADF-8C47D3AFF58C.jpg",
+    "thumb": "images/thumbs/6146A82A-D477-43AF-8ADF-8C47D3AFF58C.jpg",
+    "title": "No. 30",
+    "category": "5AMCLOUDS \u2014 FRAME",
+    "description": "",
+    "file": "6146A82A-D477-43AF-8ADF-8C47D3AFF58C.jpg"
+  },
+  {
+    "id": 31,
+    "src": "images/IMG_4699.jpg",
+    "thumb": "images/thumbs/IMG_4699.jpg",
+    "title": "No. 31",
+    "category": "5AMCLOUDS \u2014 FRAME",
+    "description": "",
+    "file": "IMG_4699.jpg"
+  },
+  {
+    "id": 32,
+    "src": "images/IMG_4709.jpg",
+    "thumb": "images/thumbs/IMG_4709.jpg",
+    "title": "No. 32",
+    "category": "5AMCLOUDS \u2014 FRAME",
+    "description": "",
+    "file": "IMG_4709.jpg"
+  },
+  {
+    "id": 33,
+    "src": "images/IMG_4760.jpg",
+    "thumb": "images/thumbs/IMG_4760.jpg",
+    "title": "No. 33",
+    "category": "5AMCLOUDS \u2014 FRAME",
+    "description": "",
+    "file": "IMG_4760.jpg"
+  },
+  {
+    "id": 34,
+    "src": "images/IMG_4765.jpg",
+    "thumb": "images/thumbs/IMG_4765.jpg",
+    "title": "No. 34",
+    "category": "5AMCLOUDS \u2014 FRAME",
+    "description": "",
+    "file": "IMG_4765.jpg"
+  },
+  {
+    "id": 35,
+    "src": "images/IMG_4772.jpg",
+    "thumb": "images/thumbs/IMG_4772.jpg",
+    "title": "No. 35",
+    "category": "5AMCLOUDS \u2014 FRAME",
+    "description": "",
+    "file": "IMG_4772.jpg"
+  },
+  {
+    "id": 36,
+    "src": "images/IMG_4779.jpg",
+    "thumb": "images/thumbs/IMG_4779.jpg",
+    "title": "No. 36",
+    "category": "5AMCLOUDS \u2014 FRAME",
+    "description": "",
+    "file": "IMG_4779.jpg"
+  },
+  {
+    "id": 37,
+    "src": "images/IMG_4781.jpg",
+    "thumb": "images/thumbs/IMG_4781.jpg",
+    "title": "No. 37",
+    "category": "5AMCLOUDS \u2014 FRAME",
+    "description": "",
+    "file": "IMG_4781.jpg"
+  },
+  {
+    "id": 38,
+    "src": "images/IMG_4796.jpg",
+    "thumb": "images/thumbs/IMG_4796.jpg",
+    "title": "No. 38",
+    "category": "5AMCLOUDS \u2014 FRAME",
+    "description": "",
+    "file": "IMG_4796.jpg"
+  },
+  {
+    "id": 39,
+    "src": "images/IMG_4805.jpg",
+    "thumb": "images/thumbs/IMG_4805.jpg",
+    "title": "No. 39",
+    "category": "5AMCLOUDS \u2014 FRAME",
+    "description": "",
+    "file": "IMG_4805.jpg"
+  },
+  {
+    "id": 40,
+    "src": "images/IMG_4822.jpg",
+    "thumb": "images/thumbs/IMG_4822.jpg",
+    "title": "No. 40",
+    "category": "5AMCLOUDS \u2014 FRAME",
+    "description": "",
+    "file": "IMG_4822.jpg"
+  },
+  {
+    "id": 41,
+    "src": "images/IMG_4879.jpg",
+    "thumb": "images/thumbs/IMG_4879.jpg",
+    "title": "No. 41",
+    "category": "5AMCLOUDS \u2014 FRAME",
+    "description": "",
+    "file": "IMG_4879.jpg"
+  },
+  {
+    "id": 42,
+    "src": "images/IMG_4880.jpg",
+    "thumb": "images/thumbs/IMG_4880.jpg",
+    "title": "No. 42",
+    "category": "5AMCLOUDS \u2014 FRAME",
+    "description": "",
+    "file": "IMG_4880.jpg"
+  },
+  {
+    "id": 43,
+    "src": "images/IMG_4909.jpg",
+    "thumb": "images/thumbs/IMG_4909.jpg",
+    "title": "No. 43",
+    "category": "5AMCLOUDS \u2014 FRAME",
+    "description": "",
+    "file": "IMG_4909.jpg"
+  },
+  {
+    "id": 44,
+    "src": "images/IMG_4958.jpg",
+    "thumb": "images/thumbs/IMG_4958.jpg",
+    "title": "No. 44",
+    "category": "5AMCLOUDS \u2014 FRAME",
+    "description": "",
+    "file": "IMG_4958.jpg"
+  },
+  {
+    "id": 45,
+    "src": "images/IMG_4961.jpg",
+    "thumb": "images/thumbs/IMG_4961.jpg",
+    "title": "No. 45",
+    "category": "5AMCLOUDS \u2014 FRAME",
+    "description": "",
+    "file": "IMG_4961.jpg"
+  },
+  {
+    "id": 46,
+    "src": "images/IMG_4977.jpg",
+    "thumb": "images/thumbs/IMG_4977.jpg",
+    "title": "No. 46",
+    "category": "5AMCLOUDS \u2014 FRAME",
+    "description": "",
+    "file": "IMG_4977.jpg"
+  },
+  {
+    "id": 47,
+    "src": "images/IMG_6207.jpg",
+    "thumb": "images/thumbs/IMG_6207.jpg",
+    "title": "No. 47",
+    "category": "5AMCLOUDS \u2014 FRAME",
+    "description": "",
+    "file": "IMG_6207.jpg"
+  },
+  {
+    "id": 48,
+    "src": "images/IMG_6210.jpg",
+    "thumb": "images/thumbs/IMG_6210.jpg",
+    "title": "No. 48",
+    "category": "5AMCLOUDS \u2014 FRAME",
+    "description": "",
+    "file": "IMG_6210.jpg"
+  },
+  {
+    "id": 49,
+    "src": "images/IMG_6227.jpg",
+    "thumb": "images/thumbs/IMG_6227.jpg",
+    "title": "No. 49",
+    "category": "5AMCLOUDS \u2014 FRAME",
+    "description": "",
+    "file": "IMG_6227.jpg"
+  },
+  {
+    "id": 50,
+    "src": "images/IMG_6376.jpg",
+    "thumb": "images/thumbs/IMG_6376.jpg",
+    "title": "No. 50",
+    "category": "5AMCLOUDS \u2014 FRAME",
+    "description": "",
+    "file": "IMG_6376.jpg"
+  },
+  {
+    "id": 51,
+    "src": "images/IMG_6477.jpg",
+    "thumb": "images/thumbs/IMG_6477.jpg",
+    "title": "No. 51",
+    "category": "5AMCLOUDS \u2014 FRAME",
+    "description": "",
+    "file": "IMG_6477.jpg"
+  },
+  {
+    "id": 52,
+    "src": "images/IMG_7134.jpg",
+    "thumb": "images/thumbs/IMG_7134.jpg",
+    "title": "No. 52",
+    "category": "5AMCLOUDS \u2014 FRAME",
+    "description": "",
+    "file": "IMG_7134.jpg"
+  },
+  {
+    "id": 53,
+    "src": "images/IMG_7222.jpg",
+    "thumb": "images/thumbs/IMG_7222.jpg",
+    "title": "No. 53",
+    "category": "5AMCLOUDS \u2014 FRAME",
+    "description": "",
+    "file": "IMG_7222.jpg"
+  },
+  {
+    "id": 54,
+    "src": "images/IMG_7505.jpg",
+    "thumb": "images/thumbs/IMG_7505.jpg",
+    "title": "No. 54",
+    "category": "5AMCLOUDS \u2014 FRAME",
+    "description": "",
+    "file": "IMG_7505.jpg"
+  },
+  {
+    "id": 55,
+    "src": "images/IMG_7508.jpg",
+    "thumb": "images/thumbs/IMG_7508.jpg",
+    "title": "No. 55",
+    "category": "5AMCLOUDS \u2014 FRAME",
+    "description": "",
+    "file": "IMG_7508.jpg"
+  },
+  {
+    "id": 56,
+    "src": "images/IMG_7529.jpg",
+    "thumb": "images/thumbs/IMG_7529.jpg",
+    "title": "No. 56",
+    "category": "5AMCLOUDS \u2014 FRAME",
+    "description": "",
+    "file": "IMG_7529.jpg"
+  },
+  {
+    "id": 57,
+    "src": "images/IMG_7551.jpg",
+    "thumb": "images/thumbs/IMG_7551.jpg",
+    "title": "No. 57",
+    "category": "5AMCLOUDS \u2014 FRAME",
+    "description": "",
+    "file": "IMG_7551.jpg"
+  },
+  {
+    "id": 58,
+    "src": "images/IMG_7585.jpg",
+    "thumb": "images/thumbs/IMG_7585.jpg",
+    "title": "No. 58",
+    "category": "5AMCLOUDS \u2014 FRAME",
+    "description": "",
+    "file": "IMG_7585.jpg"
+  },
+  {
+    "id": 59,
+    "src": "images/IMG_7592.jpg",
+    "thumb": "images/thumbs/IMG_7592.jpg",
+    "title": "No. 59",
+    "category": "5AMCLOUDS \u2014 FRAME",
+    "description": "",
+    "file": "IMG_7592.jpg"
+  },
+  {
+    "id": 60,
+    "src": "images/IMG_7624.jpg",
+    "thumb": "images/thumbs/IMG_7624.jpg",
+    "title": "No. 60",
+    "category": "5AMCLOUDS \u2014 FRAME",
+    "description": "",
+    "file": "IMG_7624.jpg"
+  },
+  {
+    "id": 61,
+    "src": "images/IMG_7773.jpg",
+    "thumb": "images/thumbs/IMG_7773.jpg",
+    "title": "No. 61",
+    "category": "5AMCLOUDS \u2014 FRAME",
+    "description": "",
+    "file": "IMG_7773.jpg"
+  },
+  {
+    "id": 62,
+    "src": "images/IMG_7855.jpg",
+    "thumb": "images/thumbs/IMG_7855.jpg",
+    "title": "No. 62",
+    "category": "5AMCLOUDS \u2014 FRAME",
+    "description": "",
+    "file": "IMG_7855.jpg"
+  },
+  {
+    "id": 63,
+    "src": "images/IMG_8142.jpg",
+    "thumb": "images/thumbs/IMG_8142.jpg",
+    "title": "No. 63",
+    "category": "5AMCLOUDS \u2014 FRAME",
+    "description": "",
+    "file": "IMG_8142.jpg"
+  },
+  {
+    "id": 64,
+    "src": "images/IMG_8145.jpg",
+    "thumb": "images/thumbs/IMG_8145.jpg",
+    "title": "No. 64",
+    "category": "5AMCLOUDS \u2014 FRAME",
+    "description": "",
+    "file": "IMG_8145.jpg"
+  }
+];
---
+++ js/gallery.js
@@ -0,0 +1,291 @@
+import * as THREE from 'three';
+
+/* ---------------- ASCII Loader ---------------- */
+var TARGET_TEXT = '5AMCLOUDS';
+var NOISE_CHARS = '.:*#%?$&+=~\u00b7\u2022\u2591\u2592\u2593';
+var INSTANT = new URLSearchParams(window.location.search).has('instant');
+var MIN_LOAD_MS = INSTANT ? 50 : 3000;
+var MAX_LOAD_MS = INSTANT ? 100 : 5000;
+
+var asciiEl = document.getElementById('ascii-text');
+var loaderEl = document.getElementById('loader');
+var loaderFillEl = document.getElementById('loader-bar-fill');
+var loaderPercentEl = document.getElementById('loader-percent');
+var siteEl = document.getElementById('site');
+
+function randomChar() {
+  return NOISE_CHARS[Math.floor(Math.random() * NOISE_CHARS.length)];
+}
+
+function renderAscii(progress) {
+  var revealCount = Math.floor(progress * TARGET_TEXT.length);
+  var out = '';
+  for (var i = 0; i < TARGET_TEXT.length; i++) {
+    if (i < revealCount) {
+      out += '<span>' + TARGET_TEXT[i] + '</span>';
+    } else {
+      out += '<span class="noise">' + randomChar() + '</span>';
+    }
+  }
+  asciiEl.innerHTML = out;
+}
+
+function setLoaderProgress(pct) {
+  var clamped = Math.max(0, Math.min(100, pct));
+  loaderFillEl.style.width = clamped + '%';
+  loaderPercentEl.textContent = Math.floor(clamped) + '%';
+  renderAscii(clamped / 100);
+}
+
+var loadProgress = 0;
+
+function runLoader(onDone) {
+  var start = performance.now();
+  var progressDisplay = 0;
+  var noiseTimer = setInterval(function () {
+    if (progressDisplay < 100) renderAscii(progressDisplay / 100);
+  }, 70);
+
+  var tickTimer = setInterval(function () {
+    var now = performance.now();
+    var elapsed = now - start;
+    var timeFraction = Math.min(1, elapsed / MIN_LOAD_MS);
+    var target = Math.min(timeFraction, Math.max(timeFraction * 0.4, loadProgress));
+    var hardCap = Math.min(1, elapsed / MAX_LOAD_MS);
+    target = Math.max(target, hardCap);
+
+    progressDisplay += (target * 100 - progressDisplay) * 0.15;
+    if (target >= 1 && progressDisplay > 98) progressDisplay = 100;
+
+    setLoaderProgress(progressDisplay);
+
+    if (progressDisplay >= 100) {
+      clearInterval(noiseTimer);
+      clearInterval(tickTimer);
+      setLoaderProgress(100);
+      setTimeout(function () {
+        loaderEl.classList.add('hidden');
+        siteEl.classList.add('visible');
+        onDone();
+      }, 250);
+    }
+  }, 40);
+}
+
+/* ---------------- Three.js Gallery ---------------- */
+var canvas = document.getElementById('scene-canvas');
+var COLS = window.innerWidth < 640 ? 3 : 8;
+var total = GALLERY_IMAGES.length;
+var ROWS = Math.ceil(total / COLS);
+
+var PLANE_W = 2.3;
+var PLANE_H = 3.05;
+var SPACING_X = PLANE_W * 1.12;
+var SPACING_Y = PLANE_H * 1.12;
+var HERO_COL = Math.floor(COLS / 2);
+var HERO_ROW = 1 % ROWS;
+var HERO_INDEX = HERO_ROW * COLS + HERO_COL;
+
+function seededFrac(i) {
+  var seed = Math.sin(i * 12.9898) * 43758.5453;
+  return seed - Math.floor(seed);
+}
+
+var scene = new THREE.Scene();
+scene.background = new THREE.Color(0xffffff);
+
+var camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 100);
+camera.position.set(0, 0, 11);
+
+var renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true, alpha: false });
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+var group = new THREE.Group();
+scene.add(group);
+
+var manager = new THREE.LoadingManager();
+manager.onProgress = function (url, loaded, itemsTotal) {
+  loadProgress = loaded / itemsTotal;
+};
+manager.onLoad = function () {
+  loadProgress = 1;
+};
+var loader = new THREE.TextureLoader(manager);
+
+var meshes = [];
+
+GALLERY_IMAGES.forEach(function (data, i) {
+  var col = i % COLS;
+  var row = Math.floor(i / COLS);
+
+  var geometry = new THREE.PlaneGeometry(PLANE_W, PLANE_H);
+  var material = new THREE.MeshBasicMaterial({ color: 0xe5e5e5 });
+  var mesh = new THREE.Mesh(geometry, material);
+
+  var centerCol = (COLS - 1) / 2;
+  var colOffset = col - centerCol;
+
+  var x = colOffset * SPACING_X;
+  var y = -row * SPACING_Y + SPACING_Y * 1.4;
+  var z = -Math.abs(colOffset) * 0.55;
+
+  var isHero = i === HERO_INDEX;
+  var tilt = (seededFrac(i) - 0.5) * 20; // -10..10 deg
+  var rotY = colOffset * -0.16;
+
+  if (isHero) {
+    mesh.position.set(x, y, z + 1.6);
+    mesh.rotation.set(0, 0, 0);
+    mesh.scale.set(1.65, 1.65, 1.65);
+  } else {
+    mesh.position.set(x, y, z);
+    mesh.rotation.set(0, rotY, THREE.MathUtils.degToRad(tilt));
+  }
+
+  mesh.userData = { index: i, baseZ: mesh.position.z, isHero: isHero };
+  group.add(mesh);
+  meshes.push(mesh);
+
+  loader.load(data.thumb, function (texture) {
+    texture.colorSpace = THREE.SRGBColorSpace;
+    var img = texture.image;
+    var imageAspect = img.width / img.height;
+    var planeAspect = PLANE_W / PLANE_H;
+    if (imageAspect > planeAspect) {
+      texture.repeat.set(planeAspect / imageAspect, 1);
+      texture.offset.set((1 - texture.repeat.x) / 2, 0);
+    } else {
+      texture.repeat.set(1, imageAspect / planeAspect);
+      texture.offset.set(0, (1 - texture.repeat.y) / 2);
+    }
+    material.map = texture;
+    material.color.set(0xffffff);
+    material.needsUpdate = true;
+  });
+});
+
+var contentHeight = ROWS * SPACING_Y;
+
+/* ---------------- Interaction ---------------- */
+var mouseNDC = new THREE.Vector2(0, 0);
+var targetRotX = 0;
+var targetRotY = 0;
+var scrollY = 0;
+var scrollTarget = 0;
+var maxScroll = Math.max(0, contentHeight - SPACING_Y * 2);
+
+var pointerDown = false;
+var dragging = false;
+var startX = 0;
+var startY = 0;
+var startScroll = 0;
+var startTime = 0;
+
+function onPointerMove(e) {
+  var nx = (e.clientX / window.innerWidth) * 2 - 1;
+  var ny = (e.clientY / window.innerHeight) * 2 - 1;
+  mouseNDC.set(nx, ny);
+  targetRotY = nx * 0.12;
+  targetRotX = -ny * 0.06;
+
+  if (pointerDown) {
+    var dx = e.clientX - startX;
+    var dy = e.clientY - startY;
+    if (Math.abs(dx) > 6 || Math.abs(dy) > 6) dragging = true;
+    scrollTarget = clamp(startScroll + dy * 1.4, -maxScroll, SPACING_Y * 0.5);
+  }
+}
+
+function onPointerDown(e) {
+  pointerDown = true;
+  dragging = false;
+  startX = e.clientX;
+  startY = e.clientY;
+  startScroll = scrollTarget;
+  startTime = performance.now();
+}
+
+function onPointerUp(e) {
+  pointerDown = false;
+  var elapsed = performance.now() - startTime;
+  if (!dragging && elapsed < 500) {
+    handleClick(e.clientX, e.clientY);
+  }
+}
+
+function onWheel(e) {
+  scrollTarget = clamp(scrollTarget - e.deltaY * 1.0, -maxScroll, SPACING_Y * 0.5);
+}
+
+function clamp(v, min, max) {
+  return Math.max(min, Math.min(max, v));
+}
+
+var raycaster = new THREE.Raycaster();
+
+function handleClick(clientX, clientY) {
+  var nx = (clientX / window.innerWidth) * 2 - 1;
+  var ny = -(clientY / window.innerHeight) * 2 + 1;
+  raycaster.setFromCamera({ x: nx, y: ny }, camera);
+  var hits = raycaster.intersectObjects(meshes, false);
+  if (hits.length > 0) {
+    openDetail(hits[0].object.userData.index);
+  }
+}
+
+canvas.addEventListener('pointermove', onPointerMove);
+canvas.addEventListener('pointerdown', onPointerDown);
+window.addEventListener('pointerup', onPointerUp);
+canvas.addEventListener('wheel', onWheel, { passive: true });
+
+window.addEventListener('resize', function () {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+/* ---------------- Detail Panel ---------------- */
+var detailEl = document.getElementById('detail');
+var detailCategory = document.getElementById('detailCategory');
+var detailTitle = document.getElementById('detailTitle');
+var detailDescription = document.getElementById('detailDescription');
+var detailFile = document.getElementById('detailFile');
+
+function openDetail(index) {
+  var data = GALLERY_IMAGES[index];
+  detailCategory.textContent = data.category || '';
+  detailTitle.textContent = data.title || '';
+  detailDescription.textContent = data.description || '';
+  detailFile.textContent = data.file || '';
+  detailEl.classList.add('open');
+}
+
+function closeDetail() {
+  detailEl.classList.remove('open');
+}
+
+document.getElementById('detailClose').addEventListener('click', closeDetail);
+document.getElementById('detailCloseBtn').addEventListener('click', closeDetail);
+document.querySelector('.detail-scrim').addEventListener('click', closeDetail);
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape') closeDetail();
+});
+
+/* ---------------- Render Loop ---------------- */
+function animate() {
+  requestAnimationFrame(animate);
+
+  scrollY += (scrollTarget - scrollY) * 0.08;
+  group.position.y = scrollY;
+
+  group.rotation.y += (targetRotY - group.rotation.y) * 0.06;
+  group.rotation.x += (targetRotX - group.rotation.x) * 0.06;
+
+  renderer.render(scene, camera);
+}
+animate();
+
+/* ---------------- Boot ---------------- */
+document.getElementById('year').textContent = new Date().getFullYear();
+runLoader(function () {});
